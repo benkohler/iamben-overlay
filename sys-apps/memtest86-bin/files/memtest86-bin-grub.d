08c721c171f9abcf691faeafce0dbf76451692dc
Ben Kohler <bkohler@gmail.com>
2016-11-01 11:05:02 -0500

    sys-apps/memtest86-bin: add info print for grub-mkconfig
    Package-Manager: portage-2.3.2

--- sys-apps/memtest86-bin/files/memtest86-bin-grub.d
+++ sys-apps/memtest86-bin/files/memtest86-bin-grub.d
@@ -5,6 +5,7 @@
 memtest_bios=/boot/memtest86-bin.bios
 
 if [ -f "${memtest_bios}" ]; then
+	echo "Found MemTest86-BIOS" >&2
 	device="$("${grub_probe}" --target=device "${memtest_bios}")"
 	path="$(make_system_path_relative_to_its_root "${memtest_bios}")"
 	cat <<EOF
@@ -22,6 +23,7 @@ fi
 memtest_efi=/boot/memtest86-bin.efi
 
 if [ -f "${memtest_efi}" ]; then
+	echo "Found MemTest86-EFI" >&2
 	device="$("${grub_probe}" --target=device "${memtest_efi}")"
 	path="$(make_system_path_relative_to_its_root "${memtest_efi}")"
 	cat <<EOF
