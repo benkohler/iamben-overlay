b4cb619e9af88b8c4a97b0527ab5bc2d45585fb8
Ben Kohler <bkohler@gentoo.org>
2018-06-25 10:39:36 -0500

    app-misc/iamben-profiled: add gentoo git aliases
    Package-Manager: Portage-2.3.40, Repoman-2.3.9
    Manifest-Sign-Key: 0xEF3B582F25416EE6

--- app-misc/iamben-profiled/files/aliases.sh
+++ app-misc/iamben-profiled/files/aliases.sh
@@ -1,3 +1,5 @@
 alias upd='emerge -uDNav world --newrepo --changed-deps'
 alias mt='qlop -tHvg'
 alias tail-emerge-logs='tail -f $(find $(portageq envvar PORTAGE_TMPDIR)/ -name build.log)'
+alias gentoo-pull="git pull --rebase=preserve origin master"
+alias gentoo-push="git push --signed origin master"
