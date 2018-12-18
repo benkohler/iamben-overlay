c73d869be3ae6c96777093c076d75238749a147b
Ben Kohler <bkohler@gentoo.org>
2018-12-18 08:22:07 -0600

    sys-apps/memtest86-bin: remove all BIOS support
    Package-Manager: Portage-2.3.52, Repoman-2.3.12
    Manifest-Sign-Key: 0x7A85BDA4D57605C7
    Signed-off-by: Ben Kohler <bkohler@gentoo.org>

--- sys-apps/memtest86-bin/files/memtest86-bin-grub.d
+++ sys-apps/memtest86-bin/files/memtest86-bin-grub.d
@@ -2,24 +2,6 @@
 
 . /usr/share/grub/grub-mkconfig_lib
 
-memtest_bios=/boot/memtest86-bin.bios
-
-if [ -f "${memtest_bios}" ]; then
-	echo "Found MemTest86-BIOS" >&2
-	device="$("${grub_probe}" --target=device "${memtest_bios}")"
-	path="$(make_system_path_relative_to_its_root "${memtest_bios}")"
-	cat <<EOF
-if [ "x\$grub_platform" = xpc ]; then
-	menuentry "MemTest86-BIOS" {
-EOF
-	prepare_grub_to_access_device "${device}" | grub_add_tab
-	cat <<EOF
-		linux16 "${path}"
-	}
-fi
-EOF
-fi
-
 memtest_efi=/boot/memtest86-bin.efi
 
 if [ -f "${memtest_efi}" ]; then
