025abfa4fe33405d21d1a0098d8c93537ce7edad
Ben Kohler <bkohler@gentoo.org>
2020-01-30 11:52:05 -0600

    app-misc/iamben-profiled: bump for new eb function
    Package-Manager: Portage-2.3.86, Repoman-2.3.20
    Signed-off-by: Ben Kohler <bkohler@gentoo.org>

--- app-misc/iamben-profiled/files/aliases.sh
+++ app-misc/iamben-profiled/files/aliases.sh
@@ -3,3 +3,7 @@ alias mt='qlop -tHvg'
 alias tail-emerge-logs='tail -f $(find $(portageq envvar PORTAGE_TMPDIR)/ -name build.log)'
 alias gentoo-pull="git pull --rebase=preserve origin master"
 alias gentoo-push="git push --signed origin master"
+
+eb() {
+	vi $(equery w -m $1)
+}
