6fde72ef4214db67ec2ea9bbed624d0e2978cdef
Ben Kohler <bkohler@gentoo.org>
2021-11-17 06:32:08 -0600

    app-misc/iamben-profiled: use correct arg this time
    Package-Manager: Portage-3.0.28, Repoman-3.0.3
    Signed-off-by: Ben Kohler <bkohler@gentoo.org>

--- app-misc/iamben-profiled/files/aliases.sh
+++ app-misc/iamben-profiled/files/aliases.sh
@@ -1,7 +1,7 @@
 alias upd='emerge -uDNav world --newrepo --changed-deps'
 alias mt='qlop -tHvg'
 alias tail-emerge-logs='tail -f $(find $(portageq envvar PORTAGE_TMPDIR)/ -name build.log)'
-alias gentoo-pull="git pull --preserve-merges origin master"
+alias gentoo-pull="git pull --rebase-merges origin master"
 alias gentoo-push="git push --signed origin master"
 
 eb() {
