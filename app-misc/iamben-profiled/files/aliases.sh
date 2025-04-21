d1d1e619a10a8c98b3b9cb9f34a3eee00fb64d1f
Ben Kohler <bkohler@gentoo.org>
2025-04-21 06:40:19 -0500

    iamben-profiled: add mynspawn alias
    Signed-off-by: Ben Kohler <bkohler@gentoo.org>

--- app-misc/iamben-profiled/files/aliases.sh
+++ app-misc/iamben-profiled/files/aliases.sh
@@ -7,3 +7,11 @@ alias gentoo-push="git push --signed origin master"
 eb() {
 	vi $(equery w -m $1)
 }
+
+mynspawn() {
+	mkdir -p ${1}/var/db/repos/gentoo
+	systemd-nspawn \
+		--bind=/var/db/repos/gentoo \
+		--bind=/var/db/repos/iamben-overlay \
+		-D ${@}
+}
